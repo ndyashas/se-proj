0b449727d1d20467f29fe3fc2c5ce00d7d4d6aae
craterkamath <vinayakkamath2010@gmail.com>
2019-11-23 19:33:18 +0530

    Unsuccessfull Test

--- onlinetest/tests/tests_student.py
+++ onlinetest/tests/tests_student.py
@@ -6,12 +6,12 @@ from onlinetest.models import studentMark, studentProfile
 
 class testStudentModels(TestCase):
 	def setUp(self):
-		studentProfile.objects.create(name = "Student 1", email = "fake-email@gmail.com",
+		studentProfile.objects.create(name="Student 1",email = "fake-email@gmail.com",
 			password = "pwd1", rollno = "number 1", client = "client1", date = datetime.datetime.now())
 		studentMark.objects.create( studentid = "st1", ques_paper_id = "paper1", marks = "12",
 			name = "Student 1", email = "fake-email@gmail.com", testtitle = "test1",
 			client = "client1", date = datetime.datetime.now(), answers = "12344", comments = "comment1___comment2")
 
-	def testCreatedObjects(self):
-		self.assertEqual(studentMark.objects.get(studentid="std1").client, "client1")
-		self.assertEqual(studentProfile.objects.get(name="Student 1").testtitle, "test1")
+	def test_studentObjects(self):
+		self.assertEqual(studentMark.objects.get(studentid="std1")["client"], "client1")
+		self.assertEqual(studentProfile.objects.get(name="Student 1")["testtitle"], "test1")
